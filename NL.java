aa597fd9fc15b9201b4311747934a28d0b78d6d3
BrendonGames <brendonvingerling@gmail.com>
2025-01-22 11:56:14 +0100

    added better layout

--- NL.java
+++ NL.java
@@ -1,18 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Mic here.
+ * Write a description of class NL here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Mic extends Actor
+public class NL extends Text
 {
     /**
-     * Act - do whatever the Mic wants to do. This method is called whenever
+     * Act - do whatever the NL wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
+        // Add your action code here.
     }
 }
