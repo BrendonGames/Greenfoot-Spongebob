47f818abb4dfe2707ee12360eef1f05aefadc6ba
Gekandor <gekandor@gmail.com>
2025-01-28 14:22:36 +0100

    plankton dodge wereld toegevoegd

---
+++ PlanktonDodge.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PlanktonDodge here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PlanktonDodge extends Minigames
+{
+
+    /**
+     * Constructor for objects of class PlanktonDodge.
+     * 
+     */
+    public PlanktonDodge()
+    {
+    }
+}
