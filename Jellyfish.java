79e856b194c514c9400ce5429df825b4fa10c648
Laynah2008 <l.i.n.schilperoord@gmail.com>
2025-02-13 19:26:18 +0100

    added code for counting jellyfish's
    added the code for the plankton dodge shit

---
+++ Jellyfish.java
@@ -0,0 +1,12 @@
+// import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Jellyfish's are the coins you can collect
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Jellyfish extends Characters
+{
+
+}
--- PlanktonDodgeSpongebob.java
+++ PlanktonDodgeSpongebob.java
@@ -8,6 +8,9 @@
  */
 public class PlanktonDodgeSpongebob extends Player
 {
+    private int Jellyfish;
+    private boolean JellyfishInUI;
+    
     /**
      * Act - do whatever the PlanktonDodgeSpongebob wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -16,6 +19,14 @@ public class PlanktonDodgeSpongebob extends Player
     {
         // Add your action code here.
         checkForJellyfish();
+        
+        // Jellyfish counter
+        JellyfishCountLocation();
+        if (!JellyfishInUI)
+        {
+            getWorld().addObject(new JellyfishAmount(), 20, 20);
+            JellyfishInUI = true;
+        }
     }
     
     /**
@@ -23,11 +34,51 @@ public class PlanktonDodgeSpongebob extends Player
      */
     public void checkForJellyfish()
     {
-        if(isTouching(Jellyfish.class))
+        if(isTouching (Jellyfish.class))
         {
             PlanktonDodge planktonDodge = (PlanktonDodge)getWorld();
             planktonDodge.addScore();
             removeTouching(Jellyfish.class);
+        }
+    }
+    
+    private void JellyfishCountLocation()
+    {
+        if (Jellyfish < 10)
+        {
+            getWorld().showText(": " + Jellyfish, 55, 20);
+            getWorld().showText("" , 60, 20);
+            getWorld().showText("" , 65, 20);
+            getWorld().showText("" , 70, 20);
+        }
+        else if (Jellyfish < 100)
+        {
+            getWorld().showText("" , 55, 20);
+            getWorld().showText(": " + Jellyfish, 60, 20);
+            getWorld().showText("" , 65, 20);
+            getWorld().showText("" , 70, 20);
+        }
+        else if (Jellyfish < 1000)
+        {
+            getWorld().showText("" , 55, 20);
+            getWorld().showText("" , 60, 20);
+            getWorld().showText(": " + Jellyfish, 65, 20);
+            getWorld().showText("" , 70, 20);
+        } 
+        else if (Jellyfish < 10000)
+        {
+            getWorld().showText("" , 55, 20);
+            getWorld().showText("" , 60, 20);
+            getWorld().showText("" , 65, 20);
+            getWorld().showText(": " + Jellyfish, 70, 20);
+        } 
+        else
+        {
+            getWorld().showText("" , 55, 20);
+            getWorld().showText("" , 60, 20);
+            getWorld().showText("" , 65, 20);
+            getWorld().showText("", 70, 20);
+            getWorld().showText(": 9999+", 75, 20);
         }
     }
 }
