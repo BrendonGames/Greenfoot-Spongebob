92a33516eb0fbe0ef0af67adfe5db83fddae5b7e
BrendonGames <brendonvingerling@gmail.com>
2025-02-14 20:17:12 +0100

    SHIT WORKS (except for settings)

--- MainMenu.java
+++ MainMenu.java
@@ -1,10 +1,20 @@
 import greenfoot.*;
 
-public class MainMenu extends Menus {
+public class MainMenu extends Menus
+{
+    int volume;
+    int gameSpeed;
+    boolean music;
+    int langId;
     private GreenfootSound menuMusic;
 
-    public MainMenu() {    
+    public MainMenu(int volumeSet, int gameSpeedSet, boolean musicSet, int languageId) {    
         super(800, 600, 1);
+
+        volume = volumeSet;
+        gameSpeed = gameSpeedSet;
+        music = musicSet;
+        langId = languageId;
         prepare();
         menuMusic = new GreenfootSound("menu_music.wav"); // Load the music
         menuMusic.playLoop(); // Play it in a loop
@@ -14,7 +24,7 @@ public class MainMenu extends Menus {
         addObject(new MenuButton("Gevonden geheimen", new SecretsWorld()), 400, 200);
         addObject(new MenuButton("Speel spel", new GaryIsMissingOutside(0,0)), 400, 280);
         addObject(new MenuButton("Vrij spel", new FreePlayWorld()), 400, 360);
-        addObject(new MenuButton("Instellingen", new SettingsWorld()), 400, 440);
+        addObject(new MenuButton("Instellingen", new SettingsWorld(volume, gameSpeed, music, langId)), 400, 440);
 
     }
 
--- SettingsWorld.java
+++ SettingsWorld.java
@@ -1,9 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class SettingsWorld extends Menus {
-    public SettingsWorld() {
+public class SettingsWorld extends Menus
+{
+    int volume;
+    int gameSpeed;
+    boolean music;
+    int langId;
+
+    public SettingsWorld(int volumeSet, int gameSpeedSet, boolean musicSet, int languageId) {
         super(800, 600, 1);
+        volume = volumeSet;
+        gameSpeed = gameSpeedSet;
+        music = musicSet;
+        langId = languageId;
         showText("Instellingen", 400, 300);
+        addObject(new MenuButton("Instellingen", new SettingsMenu(volume, gameSpeed, music, langId)), 400, 440);
     }
 }
-addObject(new MenuButton("Instellingen", new SettingsMenu()), 400, 440);
